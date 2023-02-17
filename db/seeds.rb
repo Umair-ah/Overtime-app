253d49deada7c78b7dd9c3174d5ea995e951fd0e
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-02-17 21:23:47 +0530

    seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,8 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+@user = User.create(email:"test@test.com", password:"test123", password_confirmation:"test123", first_name:"Umair", last_name:"Ahmed")
+puts "1 User created"
+
+100.times do |post|
+    Post.create(date: Date.today, rationale: "#{post}rationale content", user_id: @user.id)
+end
+
+puts "100 posts have been created"
