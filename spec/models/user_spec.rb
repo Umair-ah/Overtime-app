33d0464a7c322e68915550bc3b5cb0b57c8602c1
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-02-21 12:47:28 +0530

    method for full name and rspec test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  before do
+    @user = User.create(email:"test@test.com", password:"test123", password_confirmation:"test123", first_name:"Umair", last_name:"Ahmed")
+  end
+
   describe "creation" do
-
-    before do
-      @user = User.create(email:"test@test.com", password:"test123", password_confirmation:"test123", first_name:"Umair", last_name:"Ahmed")
-    end
-
     it "can be created" do
       expect(@user).to be_valid        
     end
@@ -16,6 +16,13 @@ RSpec.describe User, type: :model do
       @user.last_name = nil
       expect(@user).to_not be_valid      
     end
-
   end
+
+  describe "custom name method" do
+    it "will have full_name method that combines first and last name" do
+      expect(@user.full_name).to eq("UMAIR, AHMED")  
+    end
+  end
+  
+
 end
