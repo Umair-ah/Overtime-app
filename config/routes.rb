27b87f302edb6b7d74af1a0fa94c9a8dae24a8df
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-02-23 14:59:05 +0530

    implemented admin dashbaord for admins

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :posts
+      resources :users
+      resources :admin_users
+
+      root to: "posts#index"
+    end
   resources :posts
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   devise_scope :user do
     get '/users/sign_out' => 'devise/sessions#destroy'
   end
