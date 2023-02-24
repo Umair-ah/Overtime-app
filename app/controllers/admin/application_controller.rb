ea4912af1270b469a0824a29e72fe0c4d30be51e
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-02-24 07:44:09 +0530

    authorized admin dashboards for only admins and not users

--- app/controllers/admin/application_controller.rb
+++ app/controllers/admin/application_controller.rb
@@ -5,11 +5,19 @@
 # If you want to add pagination or other controller-level concerns,
 # you're free to overwrite the RESTful controller actions.
 module Admin
+  def self.admins_who_can_access 
+    ["AdminUser"]
+  end
+  
   class ApplicationController < Administrate::ApplicationController
+    before_action :authenticate_user!
     before_action :authenticate_admin
 
     def authenticate_admin
-      # TODO Add authentication logic here.
+      unless Admin.admins_who_can_access.include?(current_user.try(:type))
+        redirect_to root_path
+        flash[:notice] = "You Are Not Authorized to Perform This Action!"
+      end
     end
 
     # Override this value to specify the number of elements to display at a time
