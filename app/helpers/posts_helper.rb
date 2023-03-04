002e5443e5ef345b34a6f9a6fc8ba6c6a853ae37
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-03-04 21:44:27 +0530

    Implemented status with colors on index page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,20 @@
 module PostsHelper
+    def status_labels(status)
+        status_span_generator(status)
+    end
+
+
+    private
+        def status_span_generator(status)
+            case status
+            when "submitted"
+                content_tag(:span, status.titleize, class:"badge bg-primary")
+            
+            when "approved"
+                content_tag(:span, status.titleize, class:"badge bg-success")
+    
+            when "rejected"
+                content_tag(:span, status.titleize, class:"badge bg-danger")
+            end
+        end
 end
