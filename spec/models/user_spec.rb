2d42cf56f73cbec757af08730f0cf977add31a1e
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-03-08 14:08:06 +0530

    implemented phone validations

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe User, type: :model do
     @user = FactoryGirl.create(:user)
   end
 
-  describe "creation" do
+  describe "validations" do
     it "can be created" do
       expect(@user).to be_valid        
     end
@@ -24,6 +24,16 @@ RSpec.describe User, type: :model do
     it "cannot be created without phone number" do
       @user.phone = nil
       expect(@user).to_not be_valid      
+    end
+
+    it "can be created with integer only" do
+      @user.phone = "mygreatstr"
+      expect(@user).to_not be_valid 
+    end
+
+    it "can have only 10 chars" do
+      @user.phone = "12345678901"
+      expect(@user).to_not be_valid 
     end
   end
   
