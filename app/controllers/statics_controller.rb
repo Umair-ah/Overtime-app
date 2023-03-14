a18a21539bb193a11d4cd6682bd9c3383b16eae3
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-03-14 17:07:08 +0530

    added new style elements for audit logs inside of homepage of admins

--- app/controllers/statics_controller.rb
+++ app/controllers/statics_controller.rb
@@ -1,5 +1,6 @@
 class StaticsController < ApplicationController
     def home
         @pending_approvals = Post.where(status: "submitted")
+        @recent_audit_logs = AuditLog.last(10)
     end
 end
