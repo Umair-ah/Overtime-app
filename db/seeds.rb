e35bb41f5a0d4743feb27129e12da2160e15da1f
Umair Ahmed <umairahmed200203.umair@gmail.com>
2023-03-23 23:15:06 +0530

    implemented STI for employees via users table and skipped sms bcz i dont have twiilio

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
-@user = User.create(email:"test@user.com",
+@employee = Employee.create(email:"test@user.com",
                     password:"user123",
                     password_confirmation:"user123",
                     first_name:"Umair",
                     last_name:"Ahmed",
                     phone:"7795930967")
-puts "1 User created"
+puts "1 Employee created"
 
 AdminUser.create(email:"test@admin.com",
                 password:"admin123",
@@ -13,6 +13,13 @@ AdminUser.create(email:"test@admin.com",
                 last_name:"Ahmed",
                 phone:"7795930967")
 puts "1 Admin created"
+
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create(user_id: @employee.id, status: 0, start_date: (Date.today - 20.days))
+
+
+puts "3 audit logs have been created"
 
 100.times do |post|
     Post.create(date: Date.today, rationale: "#{post}rationale content Lorem Ipsum is simply 
@@ -23,15 +30,8 @@ puts "1 Admin created"
         remaining essentially unchanged. It was popularised in the 1960s with the
         release of Letraset sheets containing Lorem Ipsum passages, and more recently
         with desktop publishing software like Aldus PageMaker including versions of 
-        Lorem Ipsum.", overtime_request: 4.5, user_id: @user.id)
+        Lorem Ipsum.", overtime_request: 4.5, user_id: @employee.id)
 end
 
 puts "100 posts have been created"
 
-
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
-AuditLog.create(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
-
-
-puts "3 audit logs have been created"
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,10 +1,13 @@
 namespace :notification do
   desc "Sends SMS Notification to employee asking them to log if they had overtime or not"
   task sms: :environment do
-    # 1. Schedule to run at Sunday 5pm
-    # 2. Iterate over all Employees
-    # 3. Skin AdminUsers
-    # 4. Send a message that has instruction and a link to log time
+    if Time.now.sunday?
+      # 1. Schedule to run at Sunday 5pm
+      # 2. Iterate over all Employees
+      # 3. Skin AdminUsers
+      # 4. Send a message that has instruction and a link to log time
+      # SKIPPED CZ NOT GOING FOR PRODUCTION AND I DOMT HAVE TWILIO
+    end
   end
 
   desc "Sends Email Notification to admins for approve/decline overtime requests each day"
